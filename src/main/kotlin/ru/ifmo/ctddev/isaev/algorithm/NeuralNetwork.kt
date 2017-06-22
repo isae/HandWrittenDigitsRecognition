e8c4afbd3a3a2a50f6ab9b4423a5d076af902f4f
iisaev <iisaev@solanteq.com>
2017-06-22 21:44:19 +0300

    at least it is not random

--- src/main/kotlin/ru/ifmo/ctddev/isaev/algorithm/NeuralNetwork.kt
+++ src/main/kotlin/ru/ifmo/ctddev/isaev/algorithm/NeuralNetwork.kt
@@ -2,13 +2,13 @@ package ru.ifmo.ctddev.isaev.algorithm
 
 class NeuralNetwork(private val inputLayerSize: Int,
                     private val hiddenLayerSize: Int,
-                    private val outputLayerSize: Int,
+                    private val numLabels: Int,
                     private val trainingData: List<TrainObject>) {
 
-    private val theta1: Matrix = randomMatrix(hiddenLayerSize, inputLayerSize + 1)
+    private val theta1: Matrix
     // network params between input layer and hidden layer
 
-    private val theta2: Matrix = randomMatrix(outputLayerSize, hiddenLayerSize + 1)
+    private val theta2: Matrix
     // network params between hidden layer and output layer
 
     private val lambda = 0.0
@@ -16,15 +16,28 @@ class NeuralNetwork(private val inputLayerSize: Int,
     private val gradientSteps = 50
 
     init {
+        val initTheta1 = randomMatrix(hiddenLayerSize, inputLayerSize + 1)
+        val initTheta2 = randomMatrix(numLabels, hiddenLayerSize + 1)
         println("Started training data initialization")
-        val X = Matrix(trainingData.map { it.data }.toTypedArray())
+        val dataset = Matrix(trainingData.map { it.data }.toTypedArray())
         val y = trainingData.map { it.clazz }.toTypedArray()
         println("Finished training data initialization")
         val costFunction = { params: DoubleArray ->
-            nnCostFunction(params, inputLayerSize, hiddenLayerSize, outputLayerSize, X.copy(), y, lambda)
+            nnCostFunction(params, inputLayerSize, hiddenLayerSize, numLabels, dataset.copy(), y, lambda)
         }
         println("Started optimization process")
-        fmincg(costFunction, pack(theta1, theta2), gradientSteps)
+        val X = pack(initTheta1, initTheta2)
+        val (newX, fX, i) = fmincg(costFunction, X, gradientSteps)
+
+        theta1 = reshape(newX.subArray(
+                0, hiddenLayerSize * (inputLayerSize + 1)),
+                hiddenLayerSize, (inputLayerSize + 1)
+        )
+
+        theta2 = reshape(newX.subArray(
+                (hiddenLayerSize * (inputLayerSize + 1)), newX.size),
+                numLabels, (hiddenLayerSize + 1)
+        )
     }
 
     fun predictResult(example: DoubleArray): Int {
