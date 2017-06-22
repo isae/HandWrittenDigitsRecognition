25e2a122b1ce85998cfbf89cd7959b8e3ad84408
iisaev <iisaev@solanteq.com>
2017-06-22 21:33:41 +0300

    reimplemented fmincg

--- src/main/kotlin/ru/ifmo/ctddev/isaev/algorithm/fmincg.kt
+++ src/main/kotlin/ru/ifmo/ctddev/isaev/algorithm/fmincg.kt
@@ -10,7 +10,7 @@ val realmin = 2.2251e-308
 
 fun fmincg(f: (DoubleArray) -> CostGradientTuple,
            originalX: DoubleArray,
-           length: Int): DoubleArray {
+           length: Int): Triple<DoubleArray, ArrayList<Double>, Int> {
 
     var X = originalX
 
@@ -18,7 +18,7 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
 
     var i = 0                                           //zero the run length counter
     var ls_failed = false                             //no previous line search has failed
-    var fX = DoubleArray(0)
+    val fX = ArrayList<Double>()
     var (f1, df1) = f(X)
     i += getInt(length < 0)                                            //count epochs?!
     var s = -df1                                        //search direction is steepest
@@ -42,7 +42,7 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
         if (length > 0) {
             M = MAX
         } else {
-            M = Math.min(MAX, -length - i);
+            M = Math.min(MAX, -length - i)
         }
         var success = false
         var limit = -1.0                    //initialize quanteties
@@ -51,23 +51,25 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
                 limit = z1                                         //tighten the bracket
                 var z2: Double
                 if (f2 > f1) {
-                    z2 = z3 - (0.5 * d3 * z3 * z3) / (d3 * z3 + f2 - f3); }              //quadratic fit
-                else {
+                    z2 = z3 - (0.5 * d3 * z3 * z3) / (d3 * z3 + f2 - f3) //quadratic fit
+                } else {
                     val A = 6.0 * (f2 - f3) / z3 + 3.0 * (d2 + d3)                                 //cubic fit
                     val B = 3.0 * (f3 - f2) - z3 * (d3 + 2.0 * d2)
                     z2 = (Math.sqrt(B * B - A * d2 * z3 * z3) - B) / A       //numerical error possible - ok!
                 }
                 if (z2.isNaN() || z2.isInfinite()) {
-                    z2 = z3 / 2                  //if we had a numerical problem then bisect
+                    z2 = z3 / 2.0                  //if we had a numerical problem then bisect
                 }
                 z2 = Math.max(Math.min(z2, INT * z3), (1 - INT) * z3)  //don't accept too close to limits
-                z1 += z2;                                           //update the step
+                z1 += z2                                           //update the step
                 X += z2 * s
-                var (f2, df2) = f(X)
+                val (tmpf2, tmpdf2) = f(X)
+                f2 = tmpf2
+                df2 = tmpdf2
                 M -= 1
                 i += getInt(length < 0)                           //count epochs?!
                 d2 = df2.dot(s)
-                z3 = z3 - z2                    //z3 is now relative to the location of z2
+                z3 -= z2                    //z3 is now relative to the location of z2
             }
             if (f2 > (f1 + z1 * RHO * d1) || (d2 > -SIG * d1)) {
                 break                                                //this is a failure
@@ -80,11 +82,11 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
             val A = 6.0 * (f2 - f3) / z3 + 3 * (d2 + d3)                      //make cubic extrapolation
             val B = 3.0 * (f3 - f2) - z3 * (d3 + 2 * d2)
             var z2 = -d2 * z3 * z3 / (B + Math.sqrt(B * B - A * d2 * z3 * z3))       //num. error possible - ok!
-            if (!isreal(z2) || z2.isNaN() || z2.isInfinite() || z2 < 0) {   //num prob or wrong sign?
+            if (/*!isreal(z2) ||*/ z2.isNaN() || z2.isInfinite() || z2 < 0) {   //num prob or wrong sign?
                 if (limit < -0.5) {                               //if we have no upper limit
-                    z2 = z1 * (EXT - 1)                 //the extrapolate the maximum amount
+                    z2 = z1 * (EXT - 1.0)                 //the extrapolate the maximum amount
                 } else {
-                    z2 = (limit - z1) / 2                                   //otherwise bisect
+                    z2 = (limit - z1) / 2.0                                   //otherwise bisect
                 }
             } else if ((limit > -0.5) && (z2 + z1 > limit)) {          //extraplation beyond max?
                 z2 = (limit - z1) / 2.0                                               //bisect
@@ -95,18 +97,22 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
             } else if ((limit > -0.5) && (z2 < (limit - z1) * (1.0 - INT))) {   //too close to limit?
                 z2 = (limit - z1) * (1.0 - INT)
             }
-            f3 = f2 
-            d3 = d2 
+            f3 = f2
+            d3 = d2
             z3 = -z2                 //set point 3 equal to point 2
             z1 += z2
             X += z2 * s                     //update current estimates
-            (f2, df2) = f(X)
-            M = M - 1; i = i + getInt(length < 0)                             //count epochs?!
+            val (tmpf2, tmpdf2) = f(X)
+            f2 = tmpf2
+            df2 = tmpdf2
+            M -= 1
+            i += getInt(length < 0)                             //count epochs?!
             d2 = df2.dot(s)
         }                                         //end of line search
 
         if (success) {                                         //if line search succeeded
-            f1 = f2; fX = [fX' f1]';
+            f1 = f2
+            fX.add(f1)
             println("Iteration $i | Cost: $f1")
             s = (df2.dot(df2) - df1.dot(df2)) / (df1.dot(df1)) * s - df2      //Polack-Ribiere direction
             val tmp = df1
@@ -114,10 +120,10 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
             df2 = tmp                         //swap derivatives
             d2 = df1.dot(s)
             if (d2 > 0) {                                      //new slope must be negative
-                s = -df1;                              //otherwise use steepest direction
+                s = -df1                              //otherwise use steepest direction
                 d2 = -s.dot(s)
             }
-            z1 *= Math.min(RATIO, d1 / (d2 - realmin));          //slope ratio but max RATIO
+            z1 *= Math.min(RATIO, d1 / (d2 - realmin))          //slope ratio but max RATIO
             d1 = d2
             ls_failed = false                             //this line search did not fail
         } else {
@@ -132,11 +138,11 @@ fun fmincg(f: (DoubleArray) -> CostGradientTuple,
             df2 = tmp                         //swap derivatives
             s = -df1                                                    //try steepest
             d1 = -s.dot(s)
-            z1 = 1 / (1 - d1)
+            z1 = 1.0 / (1.0 - d1)
             ls_failed = true                                    //this line search failed
         }
     }
-    return X
+    return Triple(X, fX, i)
 }
 
 private operator fun Double.times(s: DoubleArray): DoubleArray {
@@ -144,15 +150,12 @@ private operator fun Double.times(s: DoubleArray): DoubleArray {
 }
 
 private operator fun DoubleArray.unaryMinus(): DoubleArray {
-
+    return this.map { -it }
+            .toDoubleArray()
 }
 
 fun getInt(cause: Boolean): Int {
     return if (cause) 1 else 0
-}
-
-fun isreal(value: Double): Boolean {
-    TODO("Ad")
 }
 
 operator fun DoubleArray.minus(other: DoubleArray): DoubleArray {
